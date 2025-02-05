63a2de518d48b730ae7c691c1ac2e16dc56dd781
oluwanifemi <kolawolemichael147@gmail.com>
2025-02-05 16:22:21 +0100

    nifemi cool

---
+++ test.js
@@ -0,0 +1,42 @@
+const nifemiData = {
+    name: 'nizzy',
+    age: 12,
+    city: 'ibadan',
+    dept: 'EEE',
+    matricno: 200912,
+}
+
+
+
+console.log(nifemiData)
+
+
+const chapel = [{
+    name: 'nizzy',
+    age: 12,
+    city: 'ibadan',
+    dept: 'EEE',
+    matricno: 200912,
+},
+{
+    name: 'joseph',
+    age: 12,
+    city: 'ibadan',
+    dept: 'EEE',
+    matricno: 200911,
+}, {
+    name: 'kola',
+    age: 12,
+    city: 'ibadan',
+    dept: 'EEE',
+    matricno: 200913,
+}, {
+    name: 'wura',
+    age: 12,
+    city: 'ibadan',
+    dept: 'EEE',
+    matricno: 200912,
+},
+]
+
+console.log(chapel[2].dept)
